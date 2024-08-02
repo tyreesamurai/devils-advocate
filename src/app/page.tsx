cd107dac4f2bf85f774220d504654e3653f03f3c
Tyree Carpenter <carpentertd@vcu.edu>
2024-08-01 20:53:14 -0400

    fix mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
-import AdvocacySection from "@/components/AdvocacySection";
 import AboutSection from "@/components/AboutSection";
 
 export default function Home() {
   return (
     <div>
       <AboutSection />
-
-      <AdvocacySection />
     </div>
   );
 }
--- src/components/QuestionSection.tsx
+++ src/components/QuestionSection.tsx
@@ -6,6 +6,7 @@ export default function QuestionSection(props: { questionSet: questionData }) {
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [selectedAnswer, setSelectedAnswer] = useState<string | null>(null);
   const [transitioning, setTransitioning] = useState(false);
+  const [isFinished, setIsFinished] = useState(false);
 
   const { topic, questions } = props.questionSet;
 
@@ -15,8 +16,6 @@ export default function QuestionSection(props: { questionSet: questionData }) {
     setSelectedAnswer((prevSelectedAnswer) =>
       prevSelectedAnswer === answer ? null : answer,
     );
-
-    console.log("Selected Answer: ", answer);
   }
 
   const handleNextQuestion = () => {
@@ -25,7 +24,11 @@ export default function QuestionSection(props: { questionSet: questionData }) {
       setTimeout(() => {
         setTransitioning(false);
         setSelectedAnswer(null);
-        setCurrentQuestionIndex((prevIndex) => prevIndex + 1);
+        if (currentQuestionIndex < questions.length - 1) {
+          setCurrentQuestionIndex((prevIndex) => prevIndex + 1);
+        } else {
+          setIsFinished(true);
+        }
       }, 300);
     }
   };
@@ -38,39 +41,45 @@ export default function QuestionSection(props: { questionSet: questionData }) {
           transitioning ? "opacity-50" : "opacity-100"
         }`}
       >
-        {currentQuestion && (
+        {isFinished ? (
           <div>
-            <h2 className="text-xl font-semibold mb-2">
-              {currentQuestion.text}
-            </h2>
-            <div className="space-y-2">
-              {currentQuestion.answers.map((answer, index) => (
-                <button
-                  key={index}
-                  onClick={() => handleAnswerSelection(answer.text)}
-                  disabled={transitioning}
-                  className={`w-full px-3 py-2 rounded ${
-                    answer.text === selectedAnswer
-                      ? "bg-blue-600 text-white"
-                      : "bg-blue-400 text-white hover:bg-blue-500"
-                  }`}
-                >
-                  {answer.text}
-                </button>
-              ))}
-            </div>
-            <button
-              onClick={handleNextQuestion}
-              disabled={selectedAnswer === null || transitioning}
-              className={`mt-4 w-full px-4 py-2 rounded ${
-                selectedAnswer === null || transitioning
-                  ? "bg-gray-400 text-white"
-                  : "bg-green-500 text-white hover:bg-green-600"
-              }`}
-            >
-              Next
-            </button>
+            <h1>You&apos;re done now!</h1>
           </div>
+        ) : (
+          currentQuestion && (
+            <div>
+              <h2 className="text-xl font-semibold mb-2">
+                {currentQuestion.text}
+              </h2>
+              <div className="space-y-2">
+                {currentQuestion.answers.map((answer, index) => (
+                  <button
+                    key={index}
+                    onClick={() => handleAnswerSelection(answer.text)}
+                    disabled={transitioning}
+                    className={`w-full px-3 py-2 rounded ${
+                      answer.text === selectedAnswer
+                        ? "bg-blue-600 text-white"
+                        : "bg-blue-400 text-white hover:bg-blue-500"
+                    }`}
+                  >
+                    {answer.text}
+                  </button>
+                ))}
+              </div>
+              <button
+                onClick={handleNextQuestion}
+                disabled={selectedAnswer === null || transitioning}
+                className={`mt-4 w-full px-4 py-2 rounded ${
+                  selectedAnswer === null || transitioning
+                    ? "bg-gray-400 text-white"
+                    : "bg-green-500 text-white hover:bg-green-600"
+                }`}
+              >
+                Next
+              </button>
+            </div>
+          )
         )}
       </div>
     </div>
